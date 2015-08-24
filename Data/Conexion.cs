9c66b6546178301162078a44e1ff3dde6fff292c
Antonio <eltony162@gmail.com>
2015-08-23 20:32:33 -0500

    Add

---
+++ Data/Conexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public class Conexion
+    {
+        public string cadenaConexion { get; set; }
+
+        public Conexion()
+        {
+            cadenaConexion = "Data Source=localhost;Initial Catalog=Customers;User ID=sa;Password=SOPTEC;Integrated Security=True;";
+        }
+
+        public DataTable ConsultaClientes(params entClientes cliente)
+        {
+            var ds = new DataTable();
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+                {
+                    using (SqlCommand comando = new SqlCommand("example", conexion))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+                        comando.Parameters.Add("@Id", SqlDbType.VarChar).Value = cliente.CustomerId;
+
+                        SqlDataAdapter da = new SqlDataAdapter { SelectCommand = comando };
+
+                        conexion.Open();
+                        comando.ExecuteNonQuery();
+                        da.Fill(ds);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return ds;
+        }
+
+    }
+}
---
+++ Data/entClientes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data
+{
+    public class entClientes
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public entClientes()
+        {
+            CustomerId = 0;
+            Name = string.Empty;
+            Country = string.Empty;
+        }
+    }
+}
